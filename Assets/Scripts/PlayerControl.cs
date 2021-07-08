eab3ee89649c537ec00560f1a06b889c6649a3b9
NamTaeHee1 <skaxogml777@naver.com>
2021-07-08 22:21:29 +0900

    [UPDATE] StageScene + PlayerControl
    [UPDATE] StageScene + PlayerControl

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -29,4 +29,14 @@ public class PlayerControl : MonoBehaviour
         if (ButtonName.Equals("Up") || ButtonName.Equals("Down"))
             v = ButtonName.Equals("Up") ? 1f : -1f;
     }
+
+    public void InputButtonDown()
+    {
+        EventSystem.current.currentSelectedGameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color32(32, 32, 255, 255);
+    }
+
+    public void InputButtonUp()
+    {
+        EventSystem.current.currentSelectedGameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.white;
+    }
 }
