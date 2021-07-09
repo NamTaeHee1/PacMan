06d04ab6fddc4942095728fd3cc5e441309a2cfe
NamTaeHee1 <skaxogml777@naver.com>
2021-07-09 21:41:14 +0900

    [UPDATE] PlayerControl
    [UPDATE] PlayerControl

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -19,10 +19,6 @@ public class PlayerControl : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Physics2D.Raycast(transform.position, new Vector2(h, 0) * 3.0f, 1.0f, LayerMask.GetMask("Map")))
-            h = 0;
-        if (Physics2D.Raycast(transform.position, new Vector2(0, v) * 3.0f, 1.0f, LayerMask.GetMask("Map")))
-            v = 0;
         PlayerTransform.Translate(new Vector2(h, v) * Time.deltaTime * MoveSpeed);
     }
 
@@ -34,10 +30,14 @@ public class PlayerControl : MonoBehaviour
         if (ButtonName.Equals("Right") || ButtonName.Equals("Left"))
         {
             h = ButtonName.Equals("Right") ? 1f : -1f;
+            if (Physics2D.Raycast(transform.position, new Vector2(h, 0) * 0.1f, 1.0f, LayerMask.GetMask("Map")))
+                h = 0;
         }
         else if (ButtonName.Equals("Up") || ButtonName.Equals("Down"))
         {
             v = ButtonName.Equals("Up") ? 1f : -1f;
+            if (Physics2D.Raycast(transform.position, new Vector2(0, v) *0.1f, 1.0f, LayerMask.GetMask("Map")))
+                v = 0;
         }
 
         SelectButtonImage.color = new Color32(32, 32, 255, 255);
