2e8f8976cf078657722406c528526d5bdbf5ab6b
NamTaeHee1 <skaxogml777@naver.com>
2021-07-09 16:03:31 +0900

    [UPDATE] PlayerControl
    [UPDATE] PlayerControl

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -20,7 +20,7 @@ public class PlayerControl : MonoBehaviour
         PlayerTransform.Translate(new Vector2(h, v) * Time.deltaTime * MoveSpeed);
     }
 
-    public void GetInputValue()
+    public void InputButtonDown()
     {
         string ButtonName = EventSystem.current.currentSelectedGameObject.name.Replace("Button", "");
 
@@ -28,10 +28,7 @@ public class PlayerControl : MonoBehaviour
             h = ButtonName.Equals("Right") ? 1f : -1f;
         if (ButtonName.Equals("Up") || ButtonName.Equals("Down"))
             v = ButtonName.Equals("Up") ? 1f : -1f;
-    }
 
-    public void InputButtonDown()
-    {
         EventSystem.current.currentSelectedGameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color32(32, 32, 255, 255);
     }
 
