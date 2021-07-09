35dcdab9fb14fb5abbdf43abc0454e7150f344f5
NamTaeHee1 <skaxogml777@naver.com>
2021-07-09 20:53:26 +0900

    [UPDATE] PlayerControl
    [UPDATE] PlayerControl

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -25,20 +25,23 @@ public class PlayerControl : MonoBehaviour
     public void InputButtonDown()
     {
         string ButtonName = EventSystem.current.currentSelectedGameObject.name.Replace("Button", "");
-
-        if()
+        SelectButtonImage = EventSystem.current.currentSelectedGameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>();
 
         if (ButtonName.Equals("Right") || ButtonName.Equals("Left"))
+        {
             h = ButtonName.Equals("Right") ? 1f : -1f;
-        if (ButtonName.Equals("Up") || ButtonName.Equals("Down"))
+        }
+        else if (ButtonName.Equals("Up") || ButtonName.Equals("Down"))
+        {
             v = ButtonName.Equals("Up") ? 1f : -1f;
+        }
 
-        EventSystem.current.currentSelectedGameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color32(32, 32, 255, 255);
+        SelectButtonImage.color = new Color32(32, 32, 255, 255);
     }
 
     public void InputButtonUp()
     {
-        EventSystem.current.currentSelectedGameObject.transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.white;
+        SelectButtonImage.color = Color.white;
         h = 0;
         v = 0;
     }
