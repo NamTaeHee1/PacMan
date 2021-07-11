8e75a4c5ce089da5d30ba86758551d1812891c0d
NamTaeHee1 <skaxogml777@naver.com>
2021-07-11 22:11:15 +0900

    [UPDATE] MainScene + ColorInfo
    [UPDATE] MainScene + ColorInfo

--- Assets/Scripts/ColorInfo.cs
+++ Assets/Scripts/ColorInfo.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu]
 public class ColorInfo : ScriptableObject
 {
-    
+    [SerializeField] private Color32 CharacterColor;
+    [SerializeField] private string ColorName;
 }
