eae94365dab3f715ce3b9cacaff4cec80010ed41
NamTaeHee1 <skaxogml777@naver.com>
2021-07-12 07:10:28 +0900

    [UPDATE] SelectButtonControl
    [UPDATE] SelectButtonControl

---
+++ Assets/Scripts/Main/Store/SelectButton/SelectButtonControl.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DanielLochner.Assets.SimpleScrollSnap {
+    public class SelectButtonControl : MonoBehaviour
+    {
+        [SerializeField] private SimpleScrollSnap CharacterColorScrollSnap;
+    }
+}
