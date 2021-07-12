aefaf8c8fb5e2ba345b36616a2a3743d61e792bf
NamTaeHee1 <skaxogml777@naver.com>
2021-07-12 11:11:25 +0900

    [UPDATE] ColorInfo
    [UPDATE] ColorInfo

--- Assets/Scripts/Main/Store/Color/ColorInfo.cs
+++ Assets/Scripts/Main/Store/Color/ColorInfo.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ColorInfo : MonoBehaviour
 {
     private SpriteRenderer CharacterColorImage;
+    private TextMeshPro CharacterColorText;
 
     public bool isHaveThisColor = false;
     public bool isSelectThisColor = false;
@@ -13,11 +15,18 @@ public class ColorInfo : MonoBehaviour
     private void Awake()
     {
         CharacterColorImage = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        CharacterColorText = transform.GetChild(1).GetComponent<TextMeshPro>();
     }
 
     public Color32 CharacterColor { 
         get { 
             return CharacterColorImage.color; 
+        }
+    }
+    
+    public string ColorName {
+        get {
+            return CharacterColorText.text;
         }
     }
 }
