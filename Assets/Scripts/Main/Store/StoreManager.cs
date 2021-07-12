2756b7bc75552583163550ec11ee9ff416165de4
NamTaeHee1 <skaxogml777@naver.com>
2021-07-12 09:19:22 +0900

    [UPDATE] StoreManager
    [UPDATE] StoreManager

--- Assets/Scripts/Main/Store/StoreManager.cs
+++ Assets/Scripts/Main/Store/StoreManager.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class StoreManager : MonoBehaviour
 {
+    public StoreManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
 
 }
