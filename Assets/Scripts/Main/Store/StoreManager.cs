df88cb377f735b8a177c0fa4b19bfaad54634699
NamTaeHee1 <skaxogml777@naver.com>
2021-07-12 14:29:31 +0900

    [UPDATE] StoreManager
    [UPDATE] StoreManager

--- Assets/Scripts/Main/Store/StoreManager.cs
+++ Assets/Scripts/Main/Store/StoreManager.cs
@@ -2,30 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoreManager : MonoBehaviour
+namespace DanielLochner.Assets.SimpleScrollSnap
 {
-    public int PointMoeny = 0;
-
-    [SerializeField] private GameObject CharacterColorContent;
-
-    public static List<ColorInfo> CharacterColorList = new List<ColorInfo>();
-    public static ColorInfo CharacterColor;
-
-    private void Awake()
+    public class StoreManager : MonoBehaviour
     {
-        CharacterColorListUpdate();
-    }
+        public int PointMoeny = 0;
 
-    private void Start()
-    {
-        CharacterColor = CharacterColorContent.transform.GetChild(0).GetComponent<ColorInfo>();
-    }
+        [SerializeField] private GameObject CharacterColorContent;
 
-    private void CharacterColorListUpdate()
-    {
-        CharacterColorList.Clear();
-        for (int i = 0; i < CharacterColorContent.transform.childCount; i++)
-            CharacterColorList.Add(CharacterColorContent.transform.GetChild(i).GetComponent<ColorInfo>());
-        Debug.Log(CharacterColorContent.transform.childCount);
+        public static List<ColorInfo> CharacterColorList = new List<ColorInfo>();
+        public static ColorInfo CharacterColor;
+
+        private void Awake()
+        {
+            CharacterColorListUpdate();
+        }
+
+        private void Start()
+        {
+            CharacterColor = CharacterColorContent.transform.GetChild(0).GetComponent<ColorInfo>();
+        }
+
+        private void CharacterColorListUpdate()
+        {
+            CharacterColorList.Clear();
+            for (int i = 0; i < CharacterColorContent.transform.childCount; i++)
+                CharacterColorList.Add(CharacterColorContent.transform.GetChild(i).GetComponent<ColorInfo>());
+        }
+
+        public void CharacterScrollSnapUpdate()
+        {
+
+        }
     }
 }
